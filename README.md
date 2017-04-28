3cde8d37bdc3d696815b8ad06287f66a1549c588
Vladimir <dosandkv@gmail.com>
2017-04-28 19:09:07 +0300

    Added endpoints with mocked data for test: * correct working of build; * correct working of "nginx" proxy;

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # movie-chooser-server
 
-This server will proxying requests to [imdb](http://www.omdbapi.com/)
+***This project was created for purpose of knowledge transfer between its participants.***  
 
-## To start project:
+This server will proxy requests to [imdb](http://www.omdbapi.com/)
+
+# Endpoints
+
+[GET] http://162.243.3.92/api/random  
+[GET] http://162.243.3.92/api/choose
+
+# Contributing
+
+To start project:
 * `npm install`
 * `npm run dev`
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 const Router = require('express').Router;
 const router = Router();
 const path = require('path');
+const marked = require('marked');
+const fs = require('fs');
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../index.html`));
+router.get('/', (req, res, next) => {
+  const pathToReadme = __dirname + '/../README.md';
+
+  fs.readFile(pathToReadme, 'utf8', (err, data) => {
+    if (err) return next(err);
+
+    res.send(marked(data.toString()));
+  });
+});
+
+router.get('/random', (req, res) => {
+  res.json({ title: 'random movie'});
+});
+
+router.get('/choose', (req, res) => {
+  res.json(
+    {
+      length: 3,
+      data: [
+        { title: 'movie-1'},
+        { title: 'movie-2'},
+        { title: 'movie-3'}
+      ]
+    }
+  );
 });
 
 module.exports = router;
